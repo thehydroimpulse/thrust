c0850ad6c29bf9b72cb0fea43a9037392b6891eb
Daniel Fagnan <dnfagnan@gmail.com>
2016-03-12 14:40:40 -0800

    Serialize common types

--- src/binary_protocol.rs
+++ src/binary_protocol.rs
@@ -1,10 +1,24 @@
 use protocol::{Serializer, ThriftSerializer};
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use byteorder;
+use std::convert;
 
 pub enum Error {
-    Noop
+    Byteorder(byteorder::Error),
+    Io(io::Error)
+}
+
+impl convert::From<byteorder::Error> for Error {
+    fn from(err: byteorder::Error) -> Error {
+        Error::Byteorder(err)
+    }
+}
+
+impl convert::From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
 }
 
 pub struct BinarySerializer<'a> {
@@ -23,59 +37,69 @@ impl<'a> Serializer for BinarySerializer<'a> {
     type Error = Error;
 
     fn serialize_bool(&mut self, val: bool) -> Result<(), Error> {
-        Ok(())
+        if val {
+            self.serialize_i8(1)
+        } else {
+            self.serialize_i8(0)
+        }
+    }
+
+    fn serialize_usize(&mut self, val: usize) -> Result<(), Error> {
+        self.serialize_isize(val as isize)
+    }
+
+    fn serialize_isize(&mut self, val: isize) -> Result<(), Error> {
+        self.serialize_i64(val as i64)
     }
 
     fn serialize_u64(&mut self, val: u64) -> Result<(), Error> {
-        Ok(())
+        self.serialize_i64(val as i64)
     }
 
     fn serialize_i64(&mut self, val: i64) -> Result<(), Error> {
+        try!(self.wr.write_i64::<BigEndian>(val));
         Ok(())
     }
 
     fn serialize_u32(&mut self, val: u32) -> Result<(), Error> {
-        Ok(())
+        self.serialize_i32(val as i32)
     }
 
     fn serialize_i32(&mut self, val: i32) -> Result<(), Error> {
+        try!(self.wr.write_i32::<BigEndian>(val));
         Ok(())
     }
 
     fn serialize_u16(&mut self, val: u16) -> Result<(), Error> {
-        Ok(())
+        self.serialize_i16(val as i16)
     }
 
     fn serialize_i16(&mut self, val: i16) -> Result<(), Error> {
+        try!(self.wr.write_i16::<BigEndian>(val));
         Ok(())
     }
 
     fn serialize_u8(&mut self, val: u8) -> Result<(), Error> {
-        Ok(())
+        self.serialize_i8(val as i8)
     }
 
     fn serialize_i8(&mut self, val: i8) -> Result<(), Error> {
+        try!(self.wr.write_i8(val));
         Ok(())
     }
 
     fn serialize_bytes(&mut self, val: &[u8]) -> Result<(), Error> {
+        self.serialize_i32(val.len() as i32);
+        try!(self.wr.write(val));
         Ok(())
     }
 
     fn serialize_str(&mut self, val: &str) -> Result<(), Error> {
-        Ok(())
+        self.serialize_bytes(val.as_bytes())
     }
 
     fn serialize_string(&mut self, val: String) -> Result<(), Error> {
-        Ok(())
-    }
-
-    fn serialize_usize(&mut self, val: usize) -> Result<(), Error> {
-        Ok(())
-    }
-
-    fn serialize_isize(&mut self, val: isize) -> Result<(), Error> {
-        Ok(())
+        self.serialize_str(&*val)
     }
 }
 
