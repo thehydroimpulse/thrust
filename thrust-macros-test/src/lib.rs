ce06322d8affe6bf3ee5335bf40ed180e8e55d11
Daniel Fagnan <dnfagnan@gmail.com>
2016-04-03 15:03:10 -0700

    Parse any next item in a loop to collect. Implement the basic derive traits on each item

--- thrust-macros-test/src/lib.rs
+++ thrust-macros-test/src/lib.rs
@@ -4,18 +4,22 @@
 thrust!("
     namespace rust foobar1
 
+    struct Flocker {
+        1: required i64 fo;
+    }
+
     enum Foo {
         HELLO,
         Foobar
     }
 
     struct Message {
-        1: required binary foobar;
-        2: optional string big;
+        1: required i32 foobar;
     }
 ");
 
 #[test]
 fn compile() {
     let m = foobar1::Foo::HELLO;
+    println!("{:?}", m);
 }
